c0a1919e2fd719aafcd4de5a62feb5768386cfd7
EnzikOFC <159156257+EnzikOFC@users.noreply.github.com>
2024-09-14 20:06:35 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -503,7 +503,7 @@ group: '「🐢」 *Esta funcion solo puede ser ejecutada en grupos.*',
 private: '「🍭」 *Esta función solo puede ser usada en chat privado.*', 
 admin: '「👑」 *Este comando solo puede ser usado por admins.*', 
 botAdmin: '「🚩」 *Para usar esta función debo ser admin.*', 
-unreg: '「🍟」 *¡(っ◔◡◔)っ ♥ Hey! no estas registrado, registrese para usar esta función ♥*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg Yurine.111*',
+unreg: '「🌸」 *¡(っ◔◡◔)っ ♥ Hey! no estas registrado, registrese para usar esta función ♥*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg Yurine.111*',
 restrict: '「💫」 *Esta característica esta desactivada.*'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
