ebe6c262a5bb861dd325806a8926bfcde27fda61
EnzikOFC <159156257+EnzikOFC@users.noreply.github.com>
2024-09-14 09:31:35 -0300

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🚩 ¡Hola! Soy Ai Yaemori, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `🚩 ¡Hola! Soy YurineBot-MD, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
 if (/^corin|corín|corinplus|corínplus|corinplushost|corínplushost|plus$/i.test(m.text)) {
 conn.reply(m.chat, `🚀 CorinPlus Hosting ¡El plus que necesitas!\n🚩 *Dash:* https://dash.corinplus.com\n🌱 *Panel:* https://ctrl.corinplus.com`, m, rcanal, )
