b89f3d0641dc2742d1f2e68707c1bb4c7ad61b8e
EnzikOFC <159156257+EnzikOFC@users.noreply.github.com>
2024-09-20 05:11:04 -0300

    Actualizar main-botreglas.js

--- plugins/main-botreglas.js
+++ plugins/main-botreglas.js
@@ -10,7 +10,7 @@ let yaemori = `🚩 *Respeta las reglas de YurineBot-MD*\n
 
 🍬 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas del bot, seras bloqueado.*
 
-⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Yaemori.*
+⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de YurineBot-MD*
 
 ${global.md}`.trim()
 await conn.reply(m.chat, yaemori, m, fake)
